c5dbf821b2060b27f3f1f7b1ca4c7e2f03d1cf1e
Sarah Lange <sarah.lange@governikus.de>
2026-07-27 10:21:46 +0200

    return supported niveau and algorithms

--- src/main/java/de/governikus/datasign/cookbook/types/response/AvailableSeals.java
+++ src/main/java/de/governikus/datasign/cookbook/types/response/AvailableSeals.java
@@ -1,9 +1,15 @@
 package de.governikus.datasign.cookbook.types.response;
 
+import de.governikus.datasign.cookbook.types.HashAlgorithm;
+import de.governikus.datasign.cookbook.types.SignatureAlgorithm;
+import de.governikus.datasign.cookbook.types.SignatureNiveau;
+
 import java.util.List;
 
 public record AvailableSeals(List<Seal> seals) {
 
-    public record Seal(String sealId, String organization, String organizationUnit, String validUntil, String error) {
+    public record Seal(String sealId, String organization, String organizationUnit, String validUntil,
+                       SignatureNiveau signatureNiveau, List<HashAlgorithm> hashAlgorithms,
+                       List<SignatureAlgorithm> signatureAlgorithms, String error) {
     }
 }
